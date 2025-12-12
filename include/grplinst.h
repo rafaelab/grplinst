85ac922525ebcf37be9cb7a58f59163b975093bb
Rafael <rafaelab@users.noreply.github.com>
2025-12-12 21:21:22 +0100

    restructure code; add CRPropa finder

---
+++ include/grplinst.h
@@ -0,0 +1,13 @@
+#ifndef GRPLINST_H
+#define GRPLINST_H
+
+
+#include "grplinst/Auxiliary.h"
+#include "grplinst/Flow.h"
+#include "grplinst/Geometry.h"
+#include "grplinst/Medium.h"
+#include "grplinst/PlasmaInstability.h"
+#include "grplinst/Simulation.h"
+
+
+#endif
